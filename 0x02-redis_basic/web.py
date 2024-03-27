e132b9cadd7830eb7390a78ff632a7077d54128d
Deey-Kerry <dianahkerrynyash@gmail.com>
2024-03-27 12:28:41 +0000

    Redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,36 +4,29 @@
 import redis
 import requests
 from functools import wraps
-r = redis.Redis()
+from typing import Callable
 
 
-def url_access_count(method):
-    """Url access that counts"""
-    @wraps(method)
-    def wrapper(url):
-        """Wraps function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-
-        key_count = "count:" + url
-        html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
-        return html_content
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that string """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
     return wrapper
 
 
-@url_access_count
+@track_get_page
 def get_page(url: str) -> str:
-    """Function that gets page"""
-    results = requests.get(url)
-    return results.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """ http request to a given page
+    """
+    response = requests.get(url)
+    return response.text
