fd06124138530840d5d6a4772066b3d854e05a14
Deey-Kerry <dianahkerrynyash@gmail.com>
2024-03-27 12:47:35 +0000

    Exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,71 +1,41 @@
 #!/usr/bin/env python3
-"""Cache class. In the __init__ method, store an instance of the
-Redis client as a private variable named _redis (using redis.Redis()) and flush the instance using flushdb.
+"""import redis"""
 
-Create a store method that takes a data argument and returns a string. The method should generate a random key (e.g. using uuid), store the input data in Redis using the random key and return the key.
-"""
+
 import redis
-from uuid import uuid4
-from typing import Union, Callable, Optional
+import requests
 from functools import wraps
-UnionOfTypes = Union[str, bytes, int, float]
+
+r = redis.Redis()
 
 
-def count_calls(method: Callable) -> Callable:
-    """Counts number of times of Cache class"""
-    key = method.__qualname__
-
+def url_access_count(method):
+    """decorator for get_page function"""
     @wraps(method)
-    def wrapper(self, *args, **kwds):
-        """ Function that wraps arguements
-        """
-        self._redis.incr(key)
-        return method(self, *args, **kwds)
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
     return wrapper
 
 
-def call_history(method: Callable) -> Callable:
-    """Calls history in the methods which are callable"""
-    input_list = method.__qualname__ + ":inputs"
-    output_list = method.__qualname__ + ":outputs"
-
-    @wraps(method)
-    def wrapper(self, *args) -> bytes:
-        """ Wrapper function for calls"""
-        self._redis.rpush(input_list, str(args))
-        output = method(self, *args)
-        self._redis.rpush(output_list, output)
-        return output
-    return wrapper
+@url_access_count
+def get_page(url: str) -> str:
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
 
 
-class Cache:
-    """Class for the cache """
-    def __init__(self):
-        """ Initializes the function """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    @count_calls
-    @call_history
-    def (self,
-              data: UnionOfTypes) -> str:
-        """Function that has Union of Types"""
-        key = str(uuid4())
-        self._redis.mset({key: data})
-        return key
-
-    def get(self,
-            key: str,
-            fn: Optional[Callable] = None) -> UnionOfTypes:
-        """gets strings for the Union of Types"""
-        data = self._redis.get(key)
-        return fn(data) if fn else data
-
-    def get_str(self, key: str) -> str:
-        """gets a string for the key"""
-        return self.get(key, str)
-
-    def get_int(self, key: str) -> int:
-        """ Gets integer"""
-        return self.get(key, int)
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
